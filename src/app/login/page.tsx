29e6646f2061c56fa4e22a89f39c0519c1732acc
antony2432 <achuquival@gmail.com>
2023-05-30 18:21:33 -0500

    Inicializando el proyecto
    - Tailwind
    - Fonts

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function page() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Landing</h1>
+      <h1>Login</h1>
     </div>
   );
 }
--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,7 +1,7 @@
 export default function page() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Landing</h1>
+      <h1 className="text-3xl">Registration</h1>
     </div>
   );
 }
