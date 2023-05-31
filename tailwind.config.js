860a502e63456cf48888cc675f840ba966f61dfc
maxihorta <maxi97raggio@gmail.com>
2023-05-31 03:18:28 -0300

    corrigiendo important

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  importante:true,
+  important:true,
   theme: {
     extend: {
       backgroundImage: {
