3bf1c1f82ecc7a4f1fde05cf2518a6ade9bf38c7
antony2432 <achuquival@gmail.com>
2023-05-31 17:39:04 -0500

    Section Post
    - SectionPost terminado
    - Responsive
    - Agreganso flex al main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import SectionPost from './components/sectionPost';
+
 export default function page() {
   return (
-    <div>
+    <main className='flex flex-col justify-center items-center gap-10 my-10'>
       <h1 className="text-3xl font-bold">Landing</h1>
-    </div>
+      <SectionPost />
+    </main>
   );
 }
