609dd89ba2d3772eaaeaa6861fdc8d303f446e67
maxihorta <maxi97raggio@gmail.com>
2023-06-01 05:54:22 -0300

    se modifico espacios de casualidad

--- src/app/components/AnimalitosRescatos/index.tsx
+++ src/app/components/AnimalitosRescatos/index.tsx
@@ -6,12 +6,12 @@ import 'slick-carousel/slick/slick-theme.css';
 import { settings } from './Settings';
 import './styles.css';
 import { Cards } from './Cards';
+
+
+
 export default function AnimalesRescatadosRecientes() {
 
 
-  
-
- 
   return (
     <section className='flex flex-col justify-center items-center w-screen  mt-24 '>
     <h2 className='text-center text-[#3C2313] text-4xl font-extrabold'>Conoce a los animalitos rescatados más recientes</h2>
@@ -19,7 +19,7 @@ export default function AnimalesRescatadosRecientes() {
     <Slider   {...settings} className=' flex flex-rows justify-center items-center  h-[450px]   w-64 min-[720px]:w-[550px] lg:w-[840px] xl:w-[1110px] '>
       
         {Cards}
-        
+
     </Slider>
     </section>
   );
--- src/app/components/slider/SliderEjemplo.tsx
+++ src/app/components/slider/SliderEjemplo.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 export default function SliderEjemplo() {
   return (
-    <div>
+    
          <div className='flex flex-col-reverse h-[440px] bg-[#FFF4E5] justify-center items-center lg:h-[771px] lg:flex-row lg:flex-wrap  '>
 
 <Image 
@@ -9,17 +9,17 @@ src='/img/PerroSlider.png'
 alt='perro'
 width='200' 
 height='180' 
-className=' top-0 opacity max-w-48 max-h-44 lg:max-w-[600px] 2xl:w-[790px] lg:max-h-[500px] 2xl:max-h-[660px] lg:w-[789px] lg:h-[652px] ' 
+className=' top-0 opacity max-w-48 max-h-52 lg:max-w-[600px] 2xl:w-[790px] lg:max-h-[500px] 2xl:max-h-[660px] lg:w-[789px] lg:h-[652px] ' 
 />
 
-<div className='flex text-base w-4/5 flex-col justify-center items-center text-center lg:w-[40%] '>
+<section className='flex text-base w-4/5 flex-col justify-center items-center text-center lg:w-[40%] '>
   <h2 className=' font-bold lg:text-4xl my-5 text-DarkBrown-700 '>Animales disponibles para adopción</h2>
   <h3 className='  my-5'>Encuentra tu compañero de vida en Puppy Pet. Adopta y brinda amor y un hogar a un animal necesitado. Juntos, hacemos la diferencia.</h3>
   <button className='hidden lg:block w-32 h-11 border-2 border-[#8E5500] bg-[#FFE6C2] rounded-lg '>Vamos</button>
   
-</div>
+</section>
 </div>
 
-    </div>
+    
   );
 }
