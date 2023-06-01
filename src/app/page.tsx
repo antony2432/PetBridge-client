dc6a5fa7d465a13cdc5170e885e7ccafb8ea2d75
maxihorta <maxi97raggio@gmail.com>
2023-06-01 06:07:24 -0300

    Agregando secciones al landing
    - slider
    - Nuestra Mision
    - Animalitos Rescatados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import SectionPost from './components/sectionPost';
 import SectionMakeDiferrence from './components/SectionMakeDiferrence';
+import SliderLanding from './components/slider';
+import NuestraMision from './components/NuestraMision';
+import AnimalesRescatadosRecientes from './components/AnimalitosRescatos';
 
 export default function page() {
   return (
     <main className='w-full flex flex-col justify-center items-center gap-10 my-10'>
-      <h1 className="text-3xl font-bold">Landing</h1>
+      
+      <SliderLanding/>
+      <NuestraMision/>
+      <AnimalesRescatadosRecientes/>
       <SectionMakeDiferrence />
       <SectionPost />
     </main>
