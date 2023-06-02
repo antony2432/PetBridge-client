0cebf27e3e00a4c4662578c45a9f3c19bd5d54df
maxihorta <maxi97raggio@gmail.com>
2023-06-02 14:44:38 -0300

    Creando el PostSend-page home
    - creo el componente
    - creo la carpeta home
    - agrego una foto de perfil de prueba

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import PostSend from '../components/PostSend';
+
+export default function Home() {
+  return (
+      <main className="w-full flex flex-col justify-center items-center gap-20 mb-10 order-2 bg-slate-400">
+       <PostSend/>
+      </main>
+  );
+}
+  
