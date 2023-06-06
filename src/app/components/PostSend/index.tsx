8241700345de2ed65acb201050482c925097c7be
maxihorta <maxi97raggio@gmail.com>
2023-06-06 01:43:27 -0300

    arreglando los estilos

--- src/app/components/PostSend/index.tsx
+++ src/app/components/PostSend/index.tsx
@@ -4,7 +4,7 @@ import ModalPostSend from './components/modalPostSend';
 export default function PostSend() {
   return (
     
-    <div className="max-w-7xl w-5/6 my-4 bg-white flex items-center gap-2  py-4 px-2 lg:pl-8 rounded-2xl shadow shadow-black 2xl:w-full">
+    <div className="max-w-5xl w-5/6 my-4 bg-white flex items-center gap-2  py-4 px-2 lg:pl-8 rounded-2xl shadow shadow-black 2xl:w-full">
       <section className="w-12 h-12 lg:w-24 lg:h-24 flex justify-center">
         <Image
           src="/img/perfil.jpg"
