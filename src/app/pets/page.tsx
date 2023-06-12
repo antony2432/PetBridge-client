ab0e019daf55d023486f083c06262f561b252dff
antony2432 <achuquival@gmail.com>
2023-06-12 15:32:24 -0500

    modificando los estilos de pets y el modal de detail

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -6,7 +6,7 @@ import Paginate from '../components/Paginate/Paginate';
 export default async function Pets() {
   return (
     <main
-      className="flex-grow flex flex-col items-center"
+      className="w-full h-full flex-grow flex flex-col items-center"
       style={{ backgroundImage: 'linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%)' }}
     >
       <FiltersPets />
