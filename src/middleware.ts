65a42192bfb37fe263540456e37f5c99935e8035
antony2432 <achuquival@gmail.com>
2023-06-11 20:01:01 -0500

    arreglando sesion cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,26 +5,22 @@ import { jwtVerify } from 'jose';
 export async function middleware(request: NextRequest) {
   const myCookie = request.cookies.get('access token');
 
-  if (request.nextUrl.pathname.includes('/home')) {
-    if (myCookie === undefined) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-
-    try {
-      const jwt = JSON.parse(myCookie!.value);
-      const prueba = await jwtVerify(
-        jwt.token,
-        new TextEncoder().encode(process.env.JWT_SECRET_KEY!),
-      );
-      console.log(prueba);
-      return NextResponse.next();
-    } catch (error) {
-      console.log(error);
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  if (myCookie === undefined) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  return NextResponse.next();
+  try {
+    const jwt = JSON.parse(myCookie!.value);
+    const prueba = await jwtVerify(
+      jwt.token,
+      new TextEncoder().encode(process.env.JWT_SECRET_KEY!),
+    );
+    console.log(prueba);
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 }
 
 export const config = {
