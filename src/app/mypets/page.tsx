3e0af840b23a41c764a2df1d79e50b94353c30d3
maxihorta <maxi97raggio@gmail.com>
2023-06-15 14:17:08 -0300

    creando la carpeta mypets

--- src/app/mypets/page.tsx
+++ src/app/mypets/page.tsx
@@ -1,20 +1,20 @@
 'use client';
 import React, { useEffect } from 'react';
-import CardPet from './cardPet';
+import CardPet from '../pets/components/cardPet';
 import { useAppSelector } from '@/redux/hook';
-import usePets from '../hook/usePets';
+import usePets from '../pets/hook/usePets';
 
 export default function CardsPets() {
   const { fetchAllPets } = usePets();
-  const paginado = useAppSelector((state) => state.paginado.componentes);
-  const Filters = useAppSelector((state) => state.paginado.Filters);
+  const { allPets } = useAppSelector((state) => state.pets);
+
   useEffect(() => {
     fetchAllPets();
   }, [fetchAllPets]);
 
   return (
     <section className="flex-grow w-full grid gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-20 md:max-w-6xl ">
-      <CardPet allPets={Filters.length ? Filters : paginado} />
+      <CardPet allPets={allPets} />
     </section>
   );
 }
