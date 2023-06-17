a17a690b672b8e24db976d86018c2b9d52cd4141
Fabian Romero <chapulinozeta@gmail.com>
2023-06-17 13:24:09 -0400

    Added styles and resetPasswordForm component

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Image from 'next/image';
 import useResetFlow from './hook/useResetFlow';
+import ResetPasswordForm from './components/ResetPaswordForm';
 
 export default function Page() {
     const {
@@ -13,13 +15,14 @@ export default function Page() {
         handlePrev,
     } = useResetFlow();
     return (
-        <main>
-            <article>
-                <div>
-                    <Image src='#' alt='animalitos coloridos'/>
+        <main className="w-full h-full flex flex-grow justify-center items-center bg-gradient-to-b from-white via-gray-100 to-gray-300">
+            <article className="w-full max-w-sm m-5 px-3 py-5 flex items-stretch bg-white rounded-md shadow-lg sm:my-10 md:py-0 md:max-w-6xl md:px-0 border">
+                <div className="flex-shrink-0 hidden md:block md:w-3/6 lg:w-4/6 md:place-self-center">
+                    <Image src="/img/register.jpeg" alt="cute pets" width={295} height={400} className='w-full'/>
                 </div>
-                <form>
-                    <h1>Cambio de Contraseña</h1>
+                <form className="flex-grow w-full max-w-sm py-5 px-5 flex flex-col gap-5 justify-center items-center md:w-3/6 md:mt-10 lg:w-2/6">
+                    <h1 className="font-extrabold text-DarkBrown-900 text-2xl">Cambio de Contraseña</h1>
+                    <ResetPasswordForm/>
                 </form>
             </article>
         </main>
