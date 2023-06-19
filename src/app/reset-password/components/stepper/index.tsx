09b805b1798f2113c7848256836478b39dbc731e
Fabian Romero <chapulinozeta@gmail.com>
2023-06-19 09:56:25 -0400

    Ahora solo se puede ir de un paso a otro por medio del botón 'siguiente'

--- src/app/reset-password/components/stepper/index.tsx
+++ src/app/reset-password/components/stepper/index.tsx
@@ -64,10 +64,10 @@ export default function StepperReset({
     return (
         <div className="w-full py-4 px-8 mt-10 flex flex-col items-center">
           <Stepper activeStep={activeStep} isLastStep={seterLast} isFirstStep={seterFirst}>
-            <Step className="h-4 w-4" onClick={() => setActiveStep(0)} />
-            <Step className="h-4 w-4" onClick={() => setActiveStep(1)} />
-            <Step className="h-4 w-4" onClick={() => setActiveStep(2)} />
-            <Step className="h-4 w-4" onClick={() => setActiveStep(3)}/>
+            <Step className="h-4 w-4" />
+            <Step className="h-4 w-4" />
+            <Step className="h-4 w-4" />
+            <Step className="h-4 w-4" />
           </Stepper>
           <div className="mt-5 flex gap-4 justify-between">
           {button}
