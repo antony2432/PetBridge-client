1fd3008a39fccdfbad813d74c5cdcadc9d9ab097
ferchu <fernandojosefantini7.0@gmail.com>
2023-06-19 18:08:13 -0300

    feature/profile

--- src/redux/thunk.ts
+++ src/redux/thunk.ts
@@ -1,13 +1,17 @@
+import  useUserSesion  from '@/hook/userSesion';
+
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-var token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJGZXJuYW5kbyIsImxhc3ROYW1lIjoiRmFudGluaSIsImVtYWlsIjoiZmVybmFuZG9qb3NlZmFudGluaTcuMEBnbWFpbC5jb20iLCJpbWFnZSI6bnVsbCwiY291bnRyeSI6bnVsbCwicGhvbmUiOm51bGwsInJvbCI6InVzZXIiLCJyZXNldCI6bnVsbCwibmV3RW1haWwiOm51bGwsInN1YiI6IjY2MDllMmI5LWRlNmEtNDA4NS05NTkzLTY2NjBhOGY1MWJmYiIsImlhdCI6MTY4NzEzNTExOSwiZXhwIjoxNjg3MjIxNTE5fQ.80jTQY-Y5fZJLS1H5tgujYo_TJ60y0JLn8g4L2NH58A';
-export const Filter = createAsyncThunk('categorias/Filter', async (obj: any) => {
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJGZXJuYW5kbyIsImxhc3ROYW1lIjoiRmFudGluaSIsImVtYWlsIjoiZmVybmFuZG9qb3NlZmFudGluaTcuMEBnbWFpbC5jb20iLCJpbWFnZSI6bnVsbCwiY291bnRyeSI6bnVsbCwicGhvbmUiOm51bGwsInJvbCI6InVzZXIiLCJyZXNldCI6bnVsbCwibmV3RW1haWwiOm51bGwsInN1YiI6ImIwYWQ4NTgxLWE2YzktNDRhNy04YjE5LTk5YmY0NWZmMWVjOSIsImlhdCI6MTY4NzIwNjIxNCwiZXhwIjoxNjg3MjkyNjE0fQ.aAF3qunlomBU2nL7hzKZD8cNkqcEquj_Tjtxmq4b22M';
+
+export const Filter = createAsyncThunk('paginado/Filter', async (obj: any) => {
+  const { sesion } = useUserSesion();
+  console.log(sesion);
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_API_BACK}/animals/specie?specie=${obj.value}`,
     {
       headers: {
-        Authorization: `Bearer${token}`,
+        Authorization: `Bearer ${token}`,
       },
     },
   );
@@ -23,14 +27,15 @@ interface Obj {
   elements: number;
 }
 
-export const Paginatee = createAsyncThunk('categorias/Paginate', async (obj: Obj) => {
+export const Paginatee = createAsyncThunk('paginado/Paginate', async (obj: Obj) => {
   console.log(obj);
+
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_API_BACK}/animals/paginate?currentPage=${obj.active}&slicePage=${obj.elements}`,
 
     {
       headers: {
-        Authorization: `Bearer${token}`,
+        Authorization: `Bearer ${token}`,
       },
     },
   );
@@ -45,6 +50,7 @@ export const GetByName = createAsyncThunk('user/Users', async (id: any) => {
   return response.data;
 });
 export const UpdateById = createAsyncThunk('user/Updatebyid', async (obj: any) => {
+
   const response = await axios.patch(
     `${process.env.NEXT_PUBLIC_API_BACK}/users/update/${obj.id}`,
     obj.file,
@@ -58,7 +64,8 @@ export const UpdateById = createAsyncThunk('user/Updatebyid', async (obj: any) =
   console.log(response.data);
   return response.data;
 });
-export const UpdateEmail = createAsyncThunk('user/Update', async (obj : any) => {
+export const UpdateEmail = createAsyncThunk('user/Update', async (obj: any) => {
+ 
   const response = await axios.patch(
     `${process.env.NEXT_PUBLIC_API_BACK}/users/change-email`,
     obj,
@@ -71,7 +78,9 @@ export const UpdateEmail = createAsyncThunk('user/Update', async (obj : any) => 
   );
   console.log(response.data);
 });
-export const UpdatePassword = createAsyncThunk('user/Update', async (obj : any) => {
+
+export const UpdatePassword = createAsyncThunk('user/Update', async (obj: any) => {
+ 
   const response = await axios.patch(
     `${process.env.NEXT_PUBLIC_API_BACK}/users/change-password`,
     obj,
