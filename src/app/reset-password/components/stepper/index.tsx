d02d3be9ad8dd8333addcbf2fe4aaed8a66d6c6d
Fabian Romero <chapulinozeta@gmail.com>
2023-06-19 09:53:33 -0400

    New logic for buttons and new step added

--- src/app/reset-password/components/stepper/index.tsx
+++ src/app/reset-password/components/stepper/index.tsx
@@ -1,6 +1,6 @@
 import { Step, Button, Stepper } from "@material-tailwind/react";
 import { IResetPasswordProps } from "../../interface/IResetPasswordProps";
-
+import { useRouter } from "next/navigation";
 
 export default function StepperReset({
     activeStep,
@@ -17,12 +17,14 @@ export default function StepperReset({
     submitToken,
 }: IResetPasswordProps) {
 
+  const router = useRouter();
+
   let button;
   switch(activeStep){
     case 0:
-      button=<Button onClick={(e)=> {
-        handleNext();
-        submitEmail(e);
+      button=<Button onClick={async (e)=> {
+        const goNext = await submitEmail(e);
+        goNext ? handleNext() : null
        }
       }
       disabled={enabled}
@@ -31,9 +33,9 @@ export default function StepperReset({
         Siguiente</Button>
       break;
     case 1:
-      button=<Button onClick={(e)=>{
-        handleNext();
-        submitToken(e);
+      button=<Button onClick={async (e)=>{
+        const goNext = await submitToken(e);
+        goNext? handleNext() : null
         }
       }
       disabled={enabled}
@@ -43,8 +45,9 @@ export default function StepperReset({
       </Button>
       break;
     case 2:
-      button=<Button onClick={(e)=>{
-        submitPassword(e);
+      button=<Button onClick={async (e)=>{
+        const goNext = await submitPassword(e);
+        goNext ? handleNext() : null
         }
       }
       disabled={enabled}
@@ -52,14 +55,19 @@ export default function StepperReset({
       className="bg-amber-500 hover:shadow-lg hover:shadow-amber-500/50">
         Crear Contraseña</Button>
     break;
+    case 3:
+      button=<Button onClick={()=> router.push('/login')} size='sm' className="bg-amber-500 hover:shadow-lg hover:shadow-amber-500/50">
+        Iniciar sesión
+      </Button>
   }
 
     return (
-        <div className="w-full py-4 px-8 mt-10">
+        <div className="w-full py-4 px-8 mt-10 flex flex-col items-center">
           <Stepper activeStep={activeStep} isLastStep={seterLast} isFirstStep={seterFirst}>
             <Step className="h-4 w-4" onClick={() => setActiveStep(0)} />
             <Step className="h-4 w-4" onClick={() => setActiveStep(1)} />
             <Step className="h-4 w-4" onClick={() => setActiveStep(2)} />
+            <Step className="h-4 w-4" onClick={() => setActiveStep(3)}/>
           </Stepper>
           <div className="mt-5 flex gap-4 justify-between">
           {button}
