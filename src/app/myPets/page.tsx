63e3ab6019d4a9c0e4f4f15f10a0dc947b0341c9
maxihorta <maxi97raggio@gmail.com>
2023-06-20 20:20:58 -0300

    modifico mypets

--- src/app/myPets/page.tsx
+++ src/app/myPets/page.tsx
@@ -2,25 +2,24 @@
 import CardsPets from './components/cardsPets';
 import FiltersPets from '../components/FiltersPets/Index';
 import Paginate from '../components/Paginate/Paginate';
-import useUserSesion from '@/hook/userSesion';
+
 
 
 export default async function Pets() {
-  const { sesion } = useUserSesion();
-  const rol = sesion?.rol;
-  console.log(rol);
+
+ 
   return (
     <main
       className="w-full h-full flex-grow flex flex-col items-center p-2"
       style={{ backgroundImage: 'linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%)' }}
     >
-    {rol === 'admin' || rol === 'fundation' ? <div><FiltersPets />  
+    <div><FiltersPets />  
       <h2 className=' font-bold text-4xl mt-5'>
         <span className='text-6xl text-DarkBrown-900'>Mis </span>
         <span className="text-5xl text-GoldenYellow-500">Mascotas</span>
          </h2>
       <CardsPets />
-      <Paginate /> </div> : <h2> No esta disponible</h2>}
+      <Paginate /> </div> 
     </main>
   );
 }
