0ea569ec0d85480d4e5c0e051246efeac695eaeb
antony <achuquival@gmail.com>
2023-06-22 14:54:25 -0500

    credencial y fetch a animals

--- src/app/pets/hook/usePets.tsx
+++ src/app/pets/hook/usePets.tsx
@@ -1,21 +1,21 @@
 import useUserSesion from '@/hook/userSesion';
 import { useAppDispatch } from '@/redux/hook';
 import { setAllPets } from '@/redux/slice/pets';
-import axios from 'axios';
 
 export default function usePets() {
   const dispatch = useAppDispatch();
   const { sesion } = useUserSesion();
   const fetchAllPets = () => {
-  
-    axios
-      .get(`${process.env.NEXT_PUBLIC_API_BACK}/animals`, {
-        headers: {
-          authorization: `Bearer ${sesion?.token}`,
-        },
-      })
-      .then((response) => {
-        dispatch(setAllPets(response.data));
+    fetch(`${process.env.NEXT_PUBLIC_API_BACK}/animals`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${sesion?.token}`,
+      },
+      credentials: 'include',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(setAllPets(data));
       })
       .catch((error) => console.log('error al cargar las mascotas ', error));
   };
