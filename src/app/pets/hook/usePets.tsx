3e9687bedf711f1b08e2848e43e14a759dbba4c9
griimreaper <leonelbehnke@gmail.com>
2023-06-23 06:16:51 -0300

    // bug fixed infinitas peticiones//

--- src/app/pets/hook/usePets.tsx
+++ src/app/pets/hook/usePets.tsx
@@ -5,19 +5,16 @@ import axios from 'axios';
 
 export default function usePets() {
   const dispatch = useAppDispatch();
-  const { sesion } = useUserSesion();
-  const fetchAllPets = () => {
-  
-    axios
-      .get(`${process.env.NEXT_PUBLIC_API_BACK}/animals`, {
-        headers: {
-          Authorization: `Bearer ${sesion?.token}`,
-        },
-      })
-      .then((response) => {
-        dispatch(setAllPets(response.data));
-      })
-      .catch((error) => console.log('error al cargar las mascotas ', error));
+
+  const fetchAllPets = async () => {
+    try {
+      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_BACK}/animals`);
+
+      dispatch(setAllPets(data));
+    } catch (error) {
+      console.log('error al cargar las mascotas ', error);
+    }
+
   };
   return {
     fetchAllPets,
